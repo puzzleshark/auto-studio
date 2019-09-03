7a199337215996110e16dfbbfb9350c09f17c88f
Sam Banning <samcbanning@gmail.com>
2019-09-02 22:23:50 -0700

    simplified script

--- auto-studio/main.py
+++ auto-studio/main.py
@@ -30,24 +30,21 @@ while True:
     if (choice == "y" or choice == "yes"):
         break
     elif (choice == "n" or choice == "no"):
-        break
-
-if (choice == 'n' or choice == "no"):
-    print("abandoned take")
-else:
-    print("loading video clip into moviepy")
-    video_clip = VideoFileClip(video_filename)
-    # video_clip = VideoFileClip("raw/mov_2019-08-25T21-02-53.mp4")
-    print("got video clip in moviepy")
-    audio_clip = AudioFileClip(audio_filename)
-    # audio_clip = AudioFileClip("raw/audio_2019-08-25T23-06-23.wav")
-    print("got clips")
-    audio_clip = audio_clip.subclip(TIME_DELAY_AUDIO_VIDEO)
-    print("chopped audio")
-    video_clip = video_clip.set_audio(audio_clip)
-    print("set the audio")
-    # add effects to video
-    video_clip = video_clip.fx(vfx.fx.fadein.fadein, duration=0.5).fx(vfx.fx.fadeout.fadeout, duration=0.5)
-    print("added effects")
-    video_clip.write_videofile(os.path.join(SAVE_FOLDER, add_time("mixed_video.mp4")))
-    print("wrote video")
+        print("abandoned take")
+        exit(0)
+print("loading video clip into moviepy")
+video_clip = VideoFileClip(video_filename)
+# video_clip = VideoFileClip("raw/mov_2019-08-25T21-02-53.mp4")
+print("got video clip in moviepy")
+audio_clip = AudioFileClip(audio_filename)
+# audio_clip = AudioFileClip("raw/audio_2019-08-25T23-06-23.wav")
+print("got clips")
+audio_clip = audio_clip.subclip(TIME_DELAY_AUDIO_VIDEO)
+print("chopped audio")
+video_clip = video_clip.set_audio(audio_clip)
+print("set the audio")
+# add effects to video
+video_clip = video_clip.fx(vfx.fx.fadein.fadein, duration=0.5).fx(vfx.fx.fadeout.fadeout, duration=0.5)
+print("added effects")
+video_clip.write_videofile(os.path.join(SAVE_FOLDER, add_time("mixed_video.mp4")))
+print("wrote video")
